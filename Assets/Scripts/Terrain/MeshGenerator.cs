757003b8b07e0cbfa76cd58ca6eb453e4c08f4e9
Palesa2323 <palesagaetsewe52@gmail.com>
2025-09-03 15:57:28 +0200

    Terrain done

--- Assets/Scripts/Terrain/MeshGenerator.cs
+++ Assets/Scripts/Terrain/MeshGenerator.cs
@@ -5,23 +5,34 @@ using System.Collections.Generic;
 [RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
 {
-
     Mesh mesh;
     Vector3[] vertices;
     int[] triangles;
+    Color[] colors;
 
     public int xSize = 20;
     public int zSize = 20;
+    public Gradient gradient;
+
+    float minTerrainHeight;
+    float maxTerrainHeight;
+
+    // Random offsets
+    float xOffset;
+    float zOffset;
+
     void Start()
     {
-       mesh = new Mesh();
+        mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
+
+        // Generate random offsets every new game
+        xOffset = Random.Range(0f, 9999f);
+        zOffset = Random.Range(0f, 9999f);
 
         CreateShape();
         UpdateMesh();
-        
     }
-  
 
     void CreateShape()
     {
@@ -33,17 +44,22 @@ public class MeshGenerator : MonoBehaviour
         {
             for (int x = 0; x <= xSize; x++)
             {
-                float y = Mathf.PerlinNoise(x*  .3f, z * .3f) * 2f; //  Perlin noise
+                float y = Mathf.PerlinNoise((x * .3f) + xOffset, (z * .3f) + zOffset) * 2f;
                 vertices[i] = new Vector3(x, y, z);
+                // Track min and max terrain height
+                minTerrainHeight = float.MaxValue;
+                maxTerrainHeight = float.MinValue;
+
+
                 i++;
             }
         }
 
         // Generate triangles
         triangles = new int[xSize * zSize * 6];
-
         int vert = 0;
         int tria = 0;
+
         for (int z = 0; z < zSize; z++)
         {
             for (int x = 0; x < xSize; x++)
@@ -56,10 +72,27 @@ public class MeshGenerator : MonoBehaviour
                 triangles[tria + 5] = vert + xSize + 2;
 
                 vert++;
-                tria += 6;      
+                tria += 6;
             }
             vert++; // Skip to next row
         }
+
+        // Generate UVs
+        colors = new Color[vertices.Length];
+        i = 0; // reset i to 0
+        for (int z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float height = Mathf.InverseLerp(minTerrainHeight, maxTerrainHeight, vertices[i].y);
+                Debug.Log(height);
+                colors[i] = gradient.Evaluate(height);
+
+                i++;
+            }
+        }
+
+
     }
 
     void UpdateMesh()
@@ -67,9 +100,9 @@ public class MeshGenerator : MonoBehaviour
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        mesh.colors =colors;
 
         mesh.RecalculateNormals();
-
     }
 
     private void OnDrawGizmos()
@@ -82,5 +115,4 @@ public class MeshGenerator : MonoBehaviour
             Gizmos.DrawSphere(vertices[i], 0.1f);
         }
     }
-
 }
