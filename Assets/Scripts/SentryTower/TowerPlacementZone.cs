57cd92513aac8809a3a814fd488a58f5baa8a4e5
Palesa2323 <palesagaetsewe52@gmail.com>
2025-09-05 14:45:40 +0200

    tower in place tbc

--- Assets/Scripts/SentryTower/TowerPlacementZone.cs
+++ Assets/Scripts/SentryTower/TowerPlacementZone.cs
@@ -2,5 +2,32 @@ using UnityEngine;
 
 public class TowerPlacementZone : MonoBehaviour
 {
-    public bool isOccupied = false;
+    [SerializeField] private Camera PlayerCamera;
+    [SerializeField] private GameObject tower; // <-- this was missing!
+
+    private GameObject CurrentPlacingTower;
+
+    void Update()
+    {
+        if (CurrentPlacingTower != null)
+        {
+            Ray camray = PlayerCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(camray, out RaycastHit hitInfo, 100))
+            {
+                CurrentPlacingTower.transform.position = hitInfo.point;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0) && CurrentPlacingTower != null)
+        {
+            // Drop the tower in place
+            CurrentPlacingTower = null;
+        }
+    }
+
+    public void SetTowerToPlace()
+    {
+        CurrentPlacingTower = Instantiate(tower, Vector3.zero, Quaternion.identity);
+    }
 }
