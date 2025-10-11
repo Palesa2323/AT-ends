15a4e4ab3114eca2a818e1229db767927a96fe2d
Palesa <palesagaetsewe52@gmail.com>
2025-10-11 14:03:12 +0200

    pooling

--- Assets/Scripts/Enemies/Scriptable/EnemySummonData.cs
+++ Assets/Scripts/Enemies/Scriptable/EnemySummonData.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New EnemySummonData", menuName = "CreateEnemySummonData")]
 public class EnemySummonData : ScriptableObject
 {
-    public GameObject EnemyPrefab;
+   public GameObject EnemyPrefab;
     public int EnemyID;
+    [Min(0)] public int poolSize = 10; // if you want pooling; ignore if not used
 }
